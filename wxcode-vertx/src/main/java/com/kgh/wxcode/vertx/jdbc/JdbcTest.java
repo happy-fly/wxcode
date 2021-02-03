1316a5ab6d26ced1f964249d2b60e4db057ec4b3
kgh <18366131816@163.com>
2021-02-03 09:04:29 +0800

    修改缩进，用2

--- wxcode-vertx/src/main/java/com/kgh/wxcode/vertx/jdbc/JdbcTest.java
+++ wxcode-vertx/src/main/java/com/kgh/wxcode/vertx/jdbc/JdbcTest.java
@@ -2,7 +2,6 @@ package com.kgh.wxcode.vertx.jdbc;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
@@ -16,76 +15,77 @@ import java.util.List;
  */
 public class JdbcTest extends AbstractVerticle {
 
-    public static void main(String[] args) {
-        Vertx.vertx().deployVerticle(new JdbcTest());
-    }
+  public static void main(String[] args) {
+    Vertx.vertx().deployVerticle(new JdbcTest());
+  }
 
-    @Override
-    public void start() throws Exception {
+  @Override
+  public void start() throws Exception {
 
-        JsonObject dbConfig = new JsonObject()
-                .put("url", "jdbc:mysql://localhost:3306/db?useSSL=false")
-                .put("driver_class", "com.mysql.cj.jdbc.Driver")
-                .put("user", "root")
-                .put("password", "root");
+    JsonObject dbConfig = new JsonObject()
+      .put("url", "jdbc:mysql://localhost:3306/db?useSSL=false")
+      .put("driver_class", "com.mysql.cj.jdbc.Driver")
+      .put("user", "root")
+      .put("password", "root");
 
-        // 创建数据库连接客户端，类似于connection
-        JDBCClient client = JDBCClient.createShared(vertx, dbConfig);
+    // 创建数据库连接客户端，类似于connection
+    JDBCClient client = JDBCClient.createShared(vertx, dbConfig);
 
-        // 新增
-        save(client);
+    // 新增
+    save(client);
 
-        // 修改
-        update(client);
+    // 修改
+    update(client);
 
-        // 查询
-        query(client);
+    // 查询
+    query(client);
 
-        // 删除
-        delete(client);
-    }
+    // 删除
+    delete(client);
+  }
 
-    private void delete(JDBCClient client) {
-        String sql = "delete from user";
-        client.update(sql, r -> {
-            if (r.succeeded()) {
-                System.out.println("删除成功");
-            }
-        });
-    }
+  private void delete(JDBCClient client) {
+    String sql = "delete from user";
+    client.update(sql, r -> {
+      if (r.succeeded()) {
+        System.out.println("删除成功");
+      }
+    });
+  }
 
-    private void update(JDBCClient client) {
-        String sql = "update user set name = ? where id = ?";
-        // 带参数
-        JsonArray params = new JsonArray()
-                .add("lisi")
-                .add(1);
-        client.updateWithParams(sql, params, r -> {
-            if (r.succeeded()) {
-                System.out.println("删除成功");
-            }
-        });
-    }
+  private void update(JDBCClient client) {
+    String sql = "update user set name = ? where id = ?";
+    // 带参数
+    JsonArray params = new JsonArray()
+      .add("lisi")
+      .add(1);
+    client.updateWithParams(sql, params, r -> {
+      if (r.succeeded()) {
+        System.out.println("删除成功");
+      }
+    });
+  }
 
-    private void save(JDBCClient client) {
-        String sql = "insert into user(name) values ('zhangsan')";
-        client.update(sql, r -> {
-            if (r.succeeded()) {
-                System.out.println("保存成功");
-            }
-        });
-    }
+  private void save(JDBCClient client) {
+    String sql = "insert into user(name) values ('zhangsan')";
+    client.update(sql, r -> {
+      if (r.succeeded()) {
+        System.out.println("保存成功");
+      }
+    });
+  }
 
-    private void query(JDBCClient client) {
-        String sql = "select * from sys_user where id = ?";
-        JsonArray params = new JsonArray().add(1);
-        client.queryWithParams(sql, params, r -> {
-            if (r.succeeded()) {
-                List<JsonObject> rows = r.result().getRows();
-                System.out.println(rows.get(0));
-            } else {
-                System.out.println(r.cause());
-            }
-        });
-    }
+  private void query(JDBCClient client) {
+    String sql = "select * from sys_user where id = ?";
+    JsonArray params = new JsonArray().add(1);
+    client.queryWithParams(sql, params, r -> {
+      if (r.succeeded()) {
+
+        List<JsonObject> rows = r.result().getRows();
+        System.out.println(rows.get(0));
+      } else {
+        System.out.println(r.cause());
+      }
+    });
+  }
 }
